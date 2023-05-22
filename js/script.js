495f1502a10aabf7ca15ba31fa8aeb42762b1330
Krystian Krupiński <krupinskikrystian22@gmail.com>
2023-05-22 09:59:52 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("hi");
+
+let button = document.querySelector(".button");
+let container = document.querySelector(".container");
+let themeName = document.querySelector(".themeName");
+
+button.addEventListener("click", () => {
+    container.classList.toggle("dark");
+
+    themeName.innerText = container.classList.contains("dark") ? "jasny" : "ciemny";
+});
